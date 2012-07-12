c627a7bfdf495b4516d64778bf2b168a4d43b0c2
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-07-12 11:27:35 -0400

    kml

---
+++ kml.js
@@ -0,0 +1,10 @@
+var m,mainLayer;
+$(function() {
+    m = new google.maps.Map(document.getElementById('map'), {
+		center:new google.maps.LatLng(42.350171,-71.084461),
+		zoom:13,
+		mapTypeId:'roadmap'
+	});
+	mainLayer = new google.maps.KmlLayer('http://calvinmetcalf.github.com/cms/brsa.kml');
+    mainLayer.setMap(m);
+});
